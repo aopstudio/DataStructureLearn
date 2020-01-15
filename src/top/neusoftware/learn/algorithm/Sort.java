e05ed8e5efaaf916e609f9a38fd7326273cbcea3
Cheng Yi <cenzhedong2@126.com>
2020-01-15 09:52:08 +0800

    增加排序算法的类，增加计数排序的实现

--- src/top/neusoftware/learn/algorithm/Sort.java
+++ src/top/neusoftware/learn/algorithm/Sort.java
@@ -1,5 +1,39 @@
 package top.neusoftware.learn.algorithm;
 
 public class Sort {
-	
+	public static void main(String[] args) {
+		int[] data= {1,5,8,7,4};
+		countingSort(data);
+		for(int i=0;i<data.length;i++) {
+			System.out.print(data[i]+" ");
+		}
+	}
+	public static void bucketSort(int[] data) {
+		
+	}
+	public static void countingSort(int[] data) {
+		int max=Integer.MIN_VALUE,min=Integer.MAX_VALUE;
+		for(int i=0;i<data.length;i++) {
+			if(data[i]>max) {
+				max=data[i];
+			}
+			if(data[i]<min) {
+				min=data[i];
+			}
+		}
+		int countingLength=max-min+1;	//计数数组的长度
+		int[] counting=new int[countingLength];
+		for(int i=0;i<data.length;i++) {	//计数
+			int index=data[i]-min;
+			counting[index]++;
+		}
+		int resultIndex=0;
+		for(int i=0;i<countingLength;i++) {
+			int frequency=counting[i];
+			for(int j=0;j<frequency;j++) {
+				//System.out.print((i+min)+" ");
+				data[resultIndex++]=i+min;
+			}
+		}
+	}
 }
